71dddc5025f5cd9ba883699c91e68d9dd5ee09e6
Liss Heidrich <31625940+Clueliss@users.noreply.github.com>
2020-05-21 23:24:54 +0200

    made gravitation stuff static

--- src/main/Simulation.java
+++ src/main/Simulation.java
@@ -11,8 +11,8 @@ public class Simulation {
 
     private boolean paused = true;
 
-    public final double GRAV_CONST = -9.807; // in m/s^2
-    public final Vec3d GRAV_VEC = new Vec3d(0, 0, GRAV_CONST);  // in m/s^2
+    public static final double GRAV_CONST = -9.807; // in m/s^2
+    public static final Vec3d GRAV_VEC = new Vec3d(0, 0, GRAV_CONST);  // in m/s^2
 
     private ArrayList<Entity> entities;
 
