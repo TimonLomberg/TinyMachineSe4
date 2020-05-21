8ecc84d8017d350246c98eeb43747e08670809d7
Liss Heidrich <31625940+Clueliss@users.noreply.github.com>
2020-05-21 17:02:00 +0200

    created polynomial3d

---
+++ src/misc/Polynomial3d.java
@@ -0,0 +1,75 @@
+package misc;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.StringJoiner;
+
+public class Polynomial3d {
+    private double[] xs;
+    private double[] ys;
+    private Polynomial3d derived = null;
+
+
+    public Polynomial3d(@NotNull double[] xs, @NotNull double[] ys) {
+        this.xs = xs;
+        this.ys = ys;
+    }
+
+    public double at(double x, double y) {
+        var sum = 0.0;
+        var minLen = Math.min(xs.length, ys.length);
+
+        for (int i = 0; i < minLen; ++i) {
+            sum += xs[i] * Math.pow(x, i);
+            sum += ys[i] * Math.pow(y, i);
+        }
+
+        if (xs.length != ys.length) {
+            var xLessY = xs.length < ys.length;
+            var rest = xLessY ? ys : xs;
+            var val = xLessY ? y : x;
+
+            for (int i = minLen; i < rest.length; ++i) {
+                sum += rest[i] * Math.pow(val, i);
+            }
+        }
+
+        return sum;
+    }
+
+    public Polynomial3d derived() {
+        if (this.derived == null) {
+            double[] newXs = new double[Math.max(0, this.xs.length-1)];
+            double[] newYs = new double[Math.max(0, this.ys.length-1)];
+
+            for (int i = 1; i < this.xs.length; ++i) {
+                newXs[i-1] = this.xs[i] * i;
+            }
+
+            for (int i = 1; i < this.ys.length; ++i) {
+                newYs[i-1] = this.ys[i] * i;
+            }
+
+            this.derived = new Polynomial3d(newXs, newYs);
+        }
+
+        return this.derived;
+    }
+
+    @Override
+    public String toString() {
+        var joiner = new StringJoiner(" + ");
+
+        for (int i = (xs.length - 1); i >= 0; --i) {
+            joiner.add("" + this.xs[i] + "x^" + i);
+        }
+
+        for (int i = (ys.length - 1); i >= 0; --i) {
+            joiner.add("" + this.ys[i] + "y^" + i);
+        }
+
+        return joiner.toString();
+    }
+
+}
