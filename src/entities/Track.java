8098a4aa7bb639bd5cd95c70d233afedc45ba0af
Liss Heidrich <31625940+Clueliss@users.noreply.github.com>
2020-05-21 21:39:07 +0200

    added normalAt

--- src/entities/Track.java
+++ src/entities/Track.java
@@ -1,29 +1,23 @@
 package entities;
 
 import main.Simulation;
+import misc.Polynomial3d;
 import misc.Vec3d;
 
 import java.util.Optional;
 
-public class Track extends Entity{
+public class Track {
+    private Polynomial3d trackFunc;
 
-    double[] trackFunction = new double[3];
-
-    public Track(Vec3d pos, Vec3d velo, Vec3d accel, double mass) {
-        super(pos, velo, accel, mass);
+    public Track(Polynomial3d trackFunc) {
+        this.trackFunc = trackFunc;
     }
 
-    @Override
-    public void performCollision(Entity other) {
+    public Vec3d normalAt(double x, double y) {
+        var v1 = new Vec3d(1, 0, this.trackFunc.derivedByXIgnoreY().at(x, 0));
+        var v2 = new Vec3d(0, 1, this.trackFunc.derivedByYIgnoreX().at(0, y));
 
+        return v1.cross(v2).norm();
     }
-
-    @Override
-    public Optional<Entity> findColliding(Simulation sim) {
-
-
-
-
-        return Optional.empty();
-    }
+    
 }
